663534ca9c6c787f8399d34428a9d70965374f0d
slees <MasterGaming4790040@gmail.com>
2022-07-09 18:38:28 -0400

    Add files via upload
    v0.1

--- core.py
+++ core.py
@@ -7,16 +7,16 @@
 #if dick_length >= 8:
 #    print(':flushed: noice bepis...')
 #
-#if dick_length > 12:
-#    print('OWOOWOWO DM ME RIGHT NOW.')
+#if dick_length > 10:
+#    print('OWOOWOWO DM ME RIGHT NOW. Slees#1162')
 # 
 #    print(type(dick_length))
 #exit()
 import os
-# Used to later generate Flavor Text
 from random import seed
 from random import choice
 from random import shuffle
+# Used to later generate Flavor Text
 
 # idk how to fucking clear the terminal lol
 print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
@@ -43,9 +43,8 @@ print('TELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nT
 
 
 # seed random number generator
-seed(1)
-random.seed()
-randomplace = ('McDonalds''Backrooms')
+
+randomplace = ('McDonalds', 'Backrooms', ' (your ip address)')
 
 print('POOF! HERE WE ARE. x:',T, 'AND y:',Z, '.THIS IS WHERE',randomplace,'IS. . .\n\n')
 
@@ -57,4 +56,7 @@ input('me: sure')
 print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 
 
-print('this is the end of the program... have a good day...')
+print('i have traveled ',x - T,' "x" units, and ',y - Z,' "y" units\n\n :D:D:D:D:D:D:D:D:D:D:D:D:D:D:D:D:DD:D:D:D:D:D:D:D::D:D:D::D')
+
+print('this is the end of the teleporting program... have a good day...\n')
+print('https://www.youtube.com/watch?v=zR6fECxF44I&list=RDMM&index=47\n this song go crazy')
