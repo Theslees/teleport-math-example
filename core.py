64ea7525ca472079902b28ea9faca35597068560
slees <MasterGaming4790040@gmail.com>
2022-07-09 16:52:03 -0400

    Add files via upload
    this took years 2 make

---
+++ core.py
@@ -0,0 +1,60 @@
+# Ignore
+#
+#dick_length = int(input('What is your dick size. (inches)\n'))
+#if dick_length < 8:
+#    print('Heh~! Smol X3')
+#
+#if dick_length >= 8:
+#    print(':flushed: noice bepis...')
+#
+#if dick_length > 12:
+#    print('OWOOWOWO DM ME RIGHT NOW.')
+# 
+#    print(type(dick_length))
+#exit()
+import os
+# Used to later generate Flavor Text
+from random import seed
+from random import choice
+from random import shuffle
+
+# idk how to fucking clear the terminal lol
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+x = (int(input('my x position:')))
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+y = (int(input('my y position:')))
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+print('YOU ARE AT x:',x, 'AND y:',y, '!\n\n\n')
+
+print('WHERE DO YOU WANT TO GO?\n\n')
+
+T = (int(input('i want to go to x:')))
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+print('YOU ARE AT x:',x, 'AND y:',y, '!\n\n\n')
+
+print('WHERE DO YOU WANT TO GO?\n\n')
+
+Z = (int(input('i also want to go to y:')))
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+
+
+
+print('TELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\nTELEPORTING. . .\n\n')
+
+
+# seed random number generator
+seed(1)
+random.seed()
+randomplace = ('McDonalds''Backrooms')
+
+print('POOF! HERE WE ARE. x:',T, 'AND y:',Z, '.THIS IS WHERE',randomplace,'IS. . .\n\n')
+
+input('me: Yeah, This place is pretty regular to me.')
+print('\n\n')
+input('LETS CALCULATE HOW LONG YOU TRAVELLED...')
+print('\n\n')
+input('me: sure')
+print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+
+
+print('this is the end of the program... have a good day...')
